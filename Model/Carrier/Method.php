9701255eaf6bdf4b7f6da4b621b0e60f4bb4d058
Harshil Patwa <harshil@mavenbird.com>
2024-07-31 07:28:15 +0000

    initial commit

---
+++ Model/Carrier/Method.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Mavenbird Free Shipping Admin Extension
+ *
+ * @category  Mavenbird
+ * @package   Mavenbird_FreeShippingAdmin
+ * @author    Mavenbird Team
+ * @copyright Copyright (c) 2024 Mavenbird (https://www.mavenbird.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+declare(strict_types=1);
+
+namespace Mavenbird\FreeShippingAdmin\Model\Carrier;
+
+use Magento\Backend\App\Area\FrontNameResolver;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\State;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory;
+use Magento\Quote\Model\Quote\Address\RateResult\MethodFactory;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+use Magento\Shipping\Model\Rate\Result;
+use Magento\Shipping\Model\Rate\ResultFactory;
+use Psr\Log\LoggerInterface;
+
+class Method extends AbstractCarrier implements CarrierInterface
+{
+    /**
+     * @var string
+     */
+    protected $_code = 'freeshippingadmin';
+
+    /**
+     * @var bool
+     */
+    protected $_isFixed = true;
+    /**
+     * Result Factories
+     *
+     * @var ResultFactory
+     */
+    protected ResultFactory $rateResultFactory;
+
+    /**
+     * Method Factories
+     *
+     * @var MethodFactory
+     */
+    protected MethodFactory $rateMethodFactory;
+
+    /**
+     * States
+     *
+     * @var State
+     */
+    protected State $appState;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param ErrorFactory         $rateErrorFactory
+     * @param LoggerInterface      $logger
+     * @param ResultFactory        $rateResultFactory
+     * @param MethodFactory        $rateMethodFactory
+     * @param State                $appState
+     * @param array                $data
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        ErrorFactory $rateErrorFactory,
+        LoggerInterface $logger,
+        ResultFactory $rateResultFactory,
+        MethodFactory $rateMethodFactory,
+        State $appState,
+        array $data = []
+    ) {
+        $this->rateResultFactory = $rateResultFactory;
+        $this->rateMethodFactory = $rateMethodFactory;
+        $this->appState = $appState;
+        parent::__construct(
+            $scopeConfig,
+            $rateErrorFactory,
+            $logger,
+            $data
+        );
+    }
+
+    /**
+     * Indicates whether the current area is admin area
+     *
+     * @return bool
+     * @throws LocalizedException
+     */
+    protected function isAdmin(): bool
+    {
+        if ($this->appState->getAreaCode() === FrontNameResolver::AREA_CODE) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * FreeShipping Rates Collector
+     *
+     * @param  RateRequest $request
+     * @return Result|bool
+     * @throws LocalizedException
+     */
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigFlag('active') || !$this->isAdmin()) {
+            return false;
+        }
+
+        $result = $this->rateResultFactory->create();
+
+        $method = $this->rateMethodFactory->create();
+
+        $method->setCarrier('freeshippingadmin');
+        $method->setCarrierTitle($this->getConfigData('title'));
+
+        $method->setMethod('freeshippingadmin');
+        $method->setMethodTitle($this->getConfigData('name'));
+
+        $method->setPrice('0.00');
+        $method->setCost('0.00');
+
+        $result->append($method);
+
+        return $result;
+    }
+
+    /**
+     * Gets Allowed Methods
+     *
+     * @return array
+     */
+    public function getAllowedMethods(): array
+    {
+        return ['freeshippingadmin' => $this->getConfigData('name')];
+    }
+}
---
+++ registration.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Mavenbird Free Shipping Admin Extension
+ *
+ * @category  Mavenbird
+ * @package   Mavenbird_FreeShippingAdmin
+ * @author    Mavenbird Team
+ * @copyright Copyright (c) 2024 Mavenbird (https://www.mavenbird.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Mavenbird_FreeShippingAdmin',
+    __DIR__
+);
